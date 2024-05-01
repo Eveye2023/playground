0faa21fdbf2fdc093f458549775e7940842972a9
Eveye2023 <yeeveen@gmail.com>
2024-05-01 14:43:17 -0400

    Add print function and adjust math worksheet template1 styles

---
+++ client/src/Components/MathWorksheetTemplate1/MathWorksheetTemplate1.tsx
@@ -0,0 +1,54 @@
+import "./MathWorksheetTemplate1.scss";
+import logo from "../../assets/images/logo7.png";
+import { range } from "lodash";
+import PrintIcon from "../PrintIcon/PrintIcon";
+
+interface MathWorksheetTemplate1Props {
+  operator: "+" | "-";
+  start: number;
+  end: number;
+}
+
+function MathWorksheetTemplate1({ operator, start, end }: MathWorksheetTemplate1Props) {
+  function randomNum() {
+    return Math.floor(Math.random() * (end + 1)) - start;
+  }
+
+  return (
+    <>
+      <main className="math-worksheet">
+        <div className="math-worksheet__print">
+          <PrintIcon />
+        </div>
+        <div className="math-worksheet__header">
+          {/* <img src={logo} alt="logo" /> */}
+          <h3>
+            Date:<span className="math-worksheet__text-holder"></span>
+          </h3>
+          <h3>
+            Name:<span className="math-worksheet__text-holder"></span>
+          </h3>
+        </div>
+        <h1 className="math-worksheet__title">
+          {operator === "+" ? "Addition " : null}
+          {operator === "-" ? "Subtraction " : null}
+          from {start} to {end}
+        </h1>
+        <div className="math-worksheet__content">
+          {range(0, 2).map((index1) => (
+            <div className="math-worksheet__column" key={index1}>
+              {range(0, 8).map((index) => (
+                <div className="math-worksheet__quest" key={index}>
+                  {randomNum()} + {randomNum()} =
+                  <span className="math-worksheet__answer-holder"> </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default MathWorksheetTemplate1;
