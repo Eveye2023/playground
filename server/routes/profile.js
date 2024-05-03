4977c0b25338da796e2b50f4a2ed6619646c3385
Eveye2023 <yeeveen@gmail.com>
2024-05-03 14:29:15 -0400

    Add server side user profile get and put endpoint

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -9,8 +9,19 @@ router.get("/", middlewares.authenticateToken, async (req, res) => {
   const { user } = req;
 
   // If token is valid, use the decoded payload, to get the info we need with the payload
-  const {id, email, name} = await knex("user").where({ id: user.id }).first();
-  res.status(200).json({id, email, name});
+  const { id, email, name } = await knex("user").where({ id: user.id }).first();
+  res.status(200).json({ id, email, name });
+});
+
+router.put("/", middlewares.authenticateToken, async (req, res) => {
+  const user = req.body;
+  try {
+    await knex("user").where({ email: user.email }).update({ name: user.name });
+    res.status(201).send("Change has been saved successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Sorry canot save the change");
+  }
 });
 
 module.exports = router;
