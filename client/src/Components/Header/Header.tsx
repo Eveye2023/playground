d6922f8efd1647cf188cec2e35baae4bc487973d
Eveye2023 <yeeveen@gmail.com>
2024-05-02 22:54:22 -0400

    Implement login authentication and logout function

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,8 +1,32 @@
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import logo from "../../assets/images/logo7.png";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
-function Header() {
+interface UserInfo {
+  id: number;
+  email: string;
+  name?: string;
+}
+interface HeaderProps {
+  user: UserInfo | null;
+}
+function Header({ user }: HeaderProps) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    window.location.reload();
+  }
   return (
     <div className="header">
       <Link to="/" className="header__link">
@@ -11,14 +35,41 @@ function Header() {
         </div>
       </Link>
       <div className="header__nav">
-        <Link to="signin" className="header__link">
+        {user ? (
+          // <Link to="profile" className="header__link">
           <div>
-            <i className="fa fa-solid fa-user"></i> Login
+            <i className="fa fa-solid fa-user"></i> {user.name || user.email}
           </div>
-        </Link>
-        <div>
+        ) : (
+          // </Link>
+          <Link to="signin" className="header__link">
+            <div>
+              <i className="fa fa-solid fa-user"></i> Login
+            </div>
+          </Link>
+        )}
+        <div className="header__menu" onClick={() => setMenuOpen(true)}>
           <i className="fa fa-solid fa-bars"></i> Menu
         </div>
+        <Drawer anchor="right" open={menuOpen} onClose={() => setMenuOpen(false)}>
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setMenuOpen(false)}
+            onKeyDown={() => setMenuOpen(false)}
+          >
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  {/* <ListItemIcon>
+                    <ExitToAppIcon />
+                  </ListItemIcon> */}
+                  <ListItemText primary="Logout" onClick={logout} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
       </div>
     </div>
   );
